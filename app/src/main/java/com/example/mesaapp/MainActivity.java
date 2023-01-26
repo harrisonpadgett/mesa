ccdacf29842899716daee143457de71937f58142
harrisonpadgett <harrisonp664@gmail.com>
2023-01-26 15:22:59 -0500

    Testing

--- app/src/main/java/com/example/mesaapp/MainActivity.java
+++ app/src/main/java/com/example/mesaapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Hello", null).show();
             }
         });
     }
