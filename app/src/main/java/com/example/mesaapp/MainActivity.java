ae63d991fa804e95d0dd521033c4424a7aeabd41
harrisonpadgett <harrisonp664@gmail.com>
2023-02-10 14:46:25 -0500

    Bottom navigation update.

--- app/src/main/java/com/example/mesaapp/MainActivity.java
+++ app/src/main/java/com/example/mesaapp/MainActivity.java
@@ -1,99 +1,51 @@
 package com.example.mesaapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.mesaapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        // Functionality of navigation menu
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Hello", null).show();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.home:
+                        return true;
+                    case R.id.contact:
+                        //startActivity(new Intent(getApplicationContext(),Contact.class));
+                        //overridePendingTransition(0,0);
+                        return true;
+                    case R.id.calendar:
+                        //startActivity(new Intent(getApplicationContext(),WeekViewActivity.class));
+                        //overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            switch(item.getItemId())
-            {
-                case R.id.home:
-                    return true;
-                case R.id.contact:
-                    startActivity(new Intent(getApplicationContext(),Contact.class));
-                    //overridePendingTransition(0,0);
-                    return true;
-                case R.id.calendar:
-                    startActivity(new Intent(getApplicationContext(),WeekViewActivity.class));
-                    //overridePendingTransition(0,0);
-                    return true;
-            }
-            return false;
-        }
-    });
 }
